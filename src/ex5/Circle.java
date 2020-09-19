6ca34caa9158f8672916f28a529bc066a643fb31
Denis Kluchanskikh <kludn@bk.ru>
2020-09-19 11:29:27 +0300

    Easy512323

--- src/ex5/Circle.java
+++ src/ex5/Circle.java
@@ -2,6 +2,7 @@ package ex5;
 
     public class Circle extends Shape {
     private double radius;
+    private double f;
 
         public Circle(String color, double radius) {
             super(color);
